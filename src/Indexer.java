eae65833a2beb4b232bb9939bc50ad051d9be668
Gina El Nesr <gina@sensciosystems.com>
2017-06-20 11:06:06 -0400

    Update

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,14 +1,8 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
