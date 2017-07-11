4e28315bbb4b3d702f47ff688d96f1108fd0a730
mjiang102628 <mjiang102628@gmail.com>
2017-07-11 17:23:00 -0400

    Commit for merge

--- DirectoryReader.java
+++ DirectoryReader.java
@@ -1,32 +1,24 @@
 import java.io.File;
-import java.util.ArrayList;
 
 public class DirectoryReader {
-static String html = "";
+	static String html = "";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		File folder = new File("C:/MICHELLE/txtfiles");
-		listFilesForFolder(folder, "");
+		listFilesForFolder(folder);
 		html += "";
 		System.out.println(html);
 	}
 
-
-public static void listFilesForFolder(File folder, String spaces) {
-	html += "<ul>";
-	//spaces += "    ";
-	for (File fileEntry : folder.listFiles()) {
-        if (fileEntry.isDirectory()) {
-        	html += "<li>" + fileEntry.getName();
-        	//System.out.println(spaces + fileEntry.getName());
-            listFilesForFolder(fileEntry, spaces);
-            html+= "</li>";
-        } 
-        //else {
-          //  System.out.println(fileEntry.getName());
-        //}
+	public static void listFilesForFolder(File folder) {
+		html += "<ul>";
+		for (File fileEntry : folder.listFiles()) {
+			if (fileEntry.isDirectory()) {
+				html += "<li>" + fileEntry.getName();
+				listFilesForFolder(fileEntry);
+				html+= "</li>";
+			} 
+		}
+		html += "</ul>";
 	}
-	html += "</ul>";
-    //spaces = spaces.substring(0, spaces.length()-4);
-}
 }
