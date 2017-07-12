bef5368c14e86832967f35a5a9ec67ffe7396c2d
mjiang102628 <mjiang102628@gmail.com>
2017-07-12 17:00:50 -0400

    Changed that I

--- Index.java
+++ Index.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import org.apache.tika.exception.TikaException;
 import org.xml.sax.SAXException;
 
-public class index{
+public class Index{
 	
 public static void main(String[] args) throws IOException, TikaException, SAXException {
 	//String indexDir = "C:/MICHELLE/txt_index";
