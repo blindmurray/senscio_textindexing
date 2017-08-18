804db379fc4f6d2f8c1937d7d23dc730f8e0336a
mjiang102628 <mjiang102628@gmail.com>
2017-08-18 10:09:09 -0400

    push

--- test-project/searchnode.js
+++ test-project/searchnode.js
@@ -3,7 +3,7 @@ var http = require("http");
 var url = require("url");
 var fs = require("fs-extra");
 var util = require("util");
-const dataDir = "/Users/Gina/Documents/Files/GitHub/senscio_textindexing/test-project/files/";
+const dataDir = "/Users/linjiang/Documents/GitHub/senscio_textindexing/test-project/files/";
 var svr = http.createServer(requesthandler);
 svr.listen(8888);
 var formidable = require("formidable");
@@ -18,12 +18,13 @@ function route(request, response, data, path) {
     //communication with search engine for search, tree, and folder add
     client.write(data + "\n");
     console.log("data recieved:" + data);
-    client.on("data", function(data) {
+    client.on("data", function (data) {
       results = data.toString();
       console.log("received:" + results);
       response.end(results);
     });
-  } else if(p > -1) {
+  }
+  else if(p > -1) {
     ext = path.slice(p + 1);
     var mimeType = {
       "html": "text/html",
@@ -54,19 +55,20 @@ function route(request, response, data, path) {
     if(mimeType[ext] !== undefined) {
       //load webpage
       var fn = path.slice(1);
-      fs.readFile(fn, function(err, content) {
+      fs.readFile(fn, function (err, content) {
         response.writeHead(200, {
           "Content-Type": mimeType[ext],
           "content-Length": content.length
         });
-        response.write(content, function() {
+        response.write(content, function () {
           setTimeout(function endit() {
             response.end();
           }, 0);
         });
       });
     }
-  } else {
+  }
+  else {
     response.end();
   }
 }
@@ -74,102 +76,102 @@ function route(request, response, data, path) {
 function requesthandler(request, response) {
   "use strict";
   switch(request.url) {
-    case "/login":
-      var mysql = require("mysql");
-      var connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "indexer"
-      });
-      console.log("reached");
-      var form = new formidable.IncomingForm();
-      form.multiples = "true";
-      form.parse(request, function(err, fields, files) {
-        var user = fields.uname;
-        var pass = fields.psw;
-        var sql = "INSERT INTO `indexer`.`account` (`username`, `userpass`) VALUES (?, ?)";
-        var values = [user, pass];
-        console.log(values);
-        var query = mysql.format(sql, values);
-        connection.query(query, function(error, results, fields) {
-          if(error) {
-            throw error;
-          }
-          console.log(error);
-        });
-      });
-      break;
-    case "/fileupload":
-      //communication with search engine for fileupload
-      var form = new formidable.IncomingForm();
-      form.multiples = "true";
-      form.parse(request, function(err, fields, files) {
-        //retrieve info from form
-        var newthing = fields.chosenFolder;
-        var filearray = files.filetoupload;
-        if(!Array.isArray(filearray)) {
-          filearray = [filearray];
+  case "/login":
+    var mysql = require("mysql");
+    var connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "indexer"
+    });
+    console.log("reached");
+    var form = new formidable.IncomingForm();
+    form.multiples = "true";
+    form.parse(request, function (err, fields, files) {
+      var user = fields.uname;
+      var pass = fields.psw;
+      var sql = "INSERT INTO `indexer`.`account` (`username`, `userpass`) VALUES (?, ?)";
+      var values = [user, pass];
+      console.log(values);
+      var query = mysql.format(sql, values);
+      connection.query(query, function (error, results, fields) {
+        if(error) {
+          throw error;
         }
-        var profile = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile()
-        var id_token = profile.getID();
-        var name = document.getElementById("addfoldername").value;
-        var path = document.getElementById("pathtoadd").value;
-        var saved = {
-          "id": "upload",
-          "filepaths": [],
-          "path_new": newthing,
-          "terms": fields.keyterms,
-          "id_token": id_token
-        };
-        filearray = filearray.map(function(file) {
-          //save all files in temporary folder
-          var f = file.name;
-          var oldpath = file.path;
-          var dup = false;
-          var npath = dataDir + f;
-          //get all filepaths
-          saved.filepaths.push(npath);
-          fs.rename(oldpath, npath, function(err) {
-            if(err) {
-              throw err;
-            }
-            return npath;
-          });
+        console.log(error);
+      });
+    });
+    break;
+  case "/fileupload":
+    //communication with search engine for fileupload
+    var form = new formidable.IncomingForm();
+    form.multiples = "true";
+    form.parse(request, function (err, fields, files) {
+      //retrieve info from form
+      var newthing = fields.chosenFolder;
+      var filearray = files.filetoupload;
+      if(!Array.isArray(filearray)) {
+        filearray = [filearray];
+      }
+      var profile = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile()
+      var id_token = profile.getID();
+      var name = document.getElementById("addfoldername").value;
+      var path = document.getElementById("pathtoadd").value;
+      var saved = {
+        "id": "upload",
+        "filepaths": [],
+        "path_new": newthing,
+        "terms": fields.keyterms,
+        "id_token": id_token
+      };
+      filearray = filearray.map(function (file) {
+        //save all files in temporary folder
+        var f = file.name;
+        var oldpath = file.path;
+        var dup = false;
+        var npath = dataDir + f;
+        //get all filepaths
+        saved.filepaths.push(npath);
+        fs.rename(oldpath, npath, function (err) {
+          if(err) {
+            throw err;
+          }
+          return npath;
         });
-        //send data
-        saved = JSON.stringify(saved);
-        console.log(client.write(saved + "\n"));
-        console.log(saved);
-        client.on("data", function(data) {
-          console.log("2" + data);
-          response.end("File(s) uploaded");
-        });
       });
-      break;
-    default:
-      var postdata = "";
-      var path = url.parse(request.url).pathname;
-      request.setEncoding("utf8");
-      request.addListener("data", function(postDataChunk) {
-        postdata += postDataChunk;
+      //send data
+      saved = JSON.stringify(saved);
+      console.log(client.write(saved + "\n"));
+      console.log(saved);
+      client.on("data", function (data) {
+        console.log("2" + data);
+        response.end("File(s) uploaded");
       });
-      // end of any data sent with the HTTP request, go to our request handler and return a webpage:
-      request.addListener("end", function() {
-        route(request, response, postdata, path);
-        //client.write(postdata + "\n");
-      });
+    });
+    break;
+  default:
+    var postdata = "";
+    var path = url.parse(request.url).pathname;
+    request.setEncoding("utf8");
+    request.addListener("data", function (postDataChunk) {
+      postdata += postDataChunk;
+    });
+    // end of any data sent with the HTTP request, go to our request handler and return a webpage:
+    request.addListener("end", function () {
+      route(request, response, postdata, path);
+      //client.write(postdata + "\n");
+    });
   }
 }
 //open socket
 console.log("sockclnt.js");
 var client = net.connect({
   port: 1221
-}, function() { //"connect" listener
+}, function () { //"connect" listener
   "use strict";
   console.log("client connected");
   client.setNoDelay();
-  client.on("end", function() {
+  client.on("end", function () {
     console.log("client disconnected");
   });
 });
